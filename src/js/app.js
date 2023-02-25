51d9a196e26083f5a58ce5271c9d17fe079187a0
Глушко Екатерина <katya_glushko_97@mail.ru>
2023-02-25 21:52:50 +0300

    first v

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,37 @@
-// TODO: write code here
+class Game {
+  setBoard() {
+    this.field = document.createElement("div");
+    this.field.classList.add("board");
+    document.body.appendChild(this.field);
+    for (let i = 0; i < 16; i += 1) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      this.field.appendChild(cell);
+    }
+  }
+
+  setGoblinPosition() {
+    let position = this.getRandom(0, 15);
+    while (position === this.lastPosition) {
+      position = this.getRandom(0, 15);
+    }
+    this.lastPosition = position;
+    return position;
+  }
+  setGoblinImg () {
+    const goblinImg = document.createElement("img");
+    goblinImg.setAttribute('src', '../img/goblin.png');
+    setInterval(()=>{
+        this.field.children[this.setGoblinPosition()].appendChild(goblinImg)
+    }, 1000)
+  }
+
+  getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+}
+
+const game = new Game();
+game.setBoard();
---
+++ webpack.common.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: 'dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'style-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        include: /img/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  devServer: {
+    overlay: true,
+    contentBase: './src',
+  },
+
+  entry: {
+    app: './src/js/app.js',
+  },
+};
